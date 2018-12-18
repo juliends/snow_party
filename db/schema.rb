0ff319d0fbf4b03839a6f2555758e26c674759c8
Dimitri Bosch <dimitribosch@hotmail.com>
2018-12-19 00:53:10 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_201044) do
+ActiveRecord::Schema.define(version: 2018_12_18_235227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id"
+    t.string "content"
+    t.boolean "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "game_answers", force: :cascade do |t|
+    t.bigint "game_id"
+    t.bigint "answer_id"
+    t.datetime "start_at"
+    t.datetime "ends_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["answer_id"], name: "index_game_answers_on_answer_id"
+    t.index ["game_id"], name: "index_game_answers_on_game_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.bigint "player_id"
+    t.bigint "quiz_id"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_games_on_player_id"
+    t.index ["quiz_id"], name: "index_games_on_quiz_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_players_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "quiz_id"
+    t.string "content"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quiz_id"], name: "index_questions_on_quiz_id"
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "reward"
+    t.string "room"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +88,11 @@ ActiveRecord::Schema.define(version: 2018_12_18_201044) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "game_answers", "answers"
+  add_foreign_key "game_answers", "games"
+  add_foreign_key "games", "players"
+  add_foreign_key "games", "quizzes"
+  add_foreign_key "questions", "quizzes"
+  add_foreign_key "quizzes", "users"
 end
