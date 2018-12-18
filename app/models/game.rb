887224f91f2f8ff4add9f64744b057cb220eb133
Dimitri Bosch <dimitribosch@hotmail.com>
2018-12-19 00:57:55 +0100

    Add relation between models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :player
   belongs_to :quiz
+  has_many :game_answers
+  has_many :answers, through: :game_answers
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,7 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :games
+  has_many :quizzes, through: :games
 end
