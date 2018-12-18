3be8653cf0384cd2c97dcffc109f3f28c332c9e1
julien da silva <julien.da.silva@hotmail.com>
2018-12-18 23:51:35 +0100

    Stimulus starter config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+// Stimulus
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context))
