0e7131bbbe623dda25949d4fcefeee900a34a867
Germain Loret <germain.loret@gmail.com>
2018-12-19 15:02:26 +0100

    Add qr_code to onboard players

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :players
   devise_for :users
   root to: 'pages#home'
-  resources :quizzes, only: [:index, :show]
+  resources :quizzes, only: [:index, :show] do
+    get "qr"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :players do
     resources :quizzes, only: :show do
