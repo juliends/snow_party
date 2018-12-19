9c4de9fe33f589fb2ade718320bdb7050d342ce1
Germain Loret <germain.loret@gmail.com>
2018-12-19 23:27:44 +0100

    Refresh leaderboard in live

--- app/controllers/players/results_controller.rb
+++ app/controllers/players/results_controller.rb
@@ -1,6 +1,8 @@
 class Players::ResultsController < Players::BaseController
+  skip_after_action :verify_policy_scoped
+
   def index
-    @game = Game.find(params[:game_id])
-    @games = policy_scope([:player, Game]).time_related(@game)
+    @quiz = Game.find(params[:game_id]).quiz
+    @games = @quiz.games.where.not(score: 0).order(score: :desc)
   end
 end
