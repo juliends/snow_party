84a5a196818715275abff7d54e02eaf227cef088
julien da silva <julien.da.silva@hotmail.com>
2018-12-19 12:24:36 +0100

    Seed player without password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-
+Answer.destroy_all
+Question.destroy_all
+Quiz.destroy_all
+User.destroy_all
+Player.destroy_all
 
 owner = User.create(email: "test@test.com", password: "123123")
-player = Player.create(email: "player@test.com", password: "123123")
+player = Player.create(email: "player@test.com")
 
 quiz = Quiz.create(
         user: owner,
@@ -17,3 +21,5 @@ quiz = Quiz.create(
     Answer.create(question: question, content: "This is answer false", correct: false)
   end
 end
+
+puts "Seeded 🌱"
