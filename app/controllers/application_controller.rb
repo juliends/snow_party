11485caa37a05e663f575e70b1c2a399ac13c8d2
julien da silva <julien.da.silva@hotmail.com>
2018-12-19 14:13:57 +0100

    Removed useless players devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # Only for sign in/up of User
   def after_sign_in_path_for(resource)
     if resource.class == User
       quizzes_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :players, controllers: {
-    sessions: 'players/sessions',
     registrations: 'players/registrations'
   }
 
