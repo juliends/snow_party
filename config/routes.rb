2f8b6f9476e26626aff8f3a66588fe1466d4a05c
julien da silva <julien.da.silva@hotmail.com>
2018-12-19 12:46:53 +0100

    quizz params on player signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :players
   devise_for :users
+  devise_for :players, controllers: {
+    sessions: 'players/sessions',
+    registrations: 'players/registrations'
+  }
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
