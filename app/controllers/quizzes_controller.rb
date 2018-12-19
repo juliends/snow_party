8473cdd0e232196eda68b18cdd1fce8a216bec7f
Germain Loret <germain.loret@gmail.com>
2018-12-19 12:58:28 +0100

    Create quiz show for owner

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,8 +1,19 @@
 class QuizzesController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
+  before_action :set_desktop_display
 
   def index
-    @desktop = true
     @quizzes = current_user.quizzes
+  end
+
+  def show
+    @quiz = Quiz.find(params[:id])
+    authorize @quiz
+  end
+
+  private
+
+  def set_desktop_display
+    @desktop = true
   end
 end
