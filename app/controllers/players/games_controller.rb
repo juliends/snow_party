f7fb5a69d8d2ccd148e37a9e326cedb56328db41
Dimitri Bosch <dimitribosch@hotmail.com>
2018-12-19 12:59:17 +0100

    Add game creation feature

---
+++ app/controllers/players/games_controller.rb
@@ -0,0 +1,17 @@
+class Players::GamesController < Players::BaseController
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    @game = Game.new(quiz: @quiz, player: current_player)
+    authorize [:player, @game]
+    if @game.save
+      redirect_to players_game_path @game
+    else
+      render "players/quizzes/show"
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    authorize [:player, @game]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :players do
-    resources :quizzes, only: :show
+    resources :quizzes, only: :show do
+      resources :games, only: [:create]
+    end
+    resources :games, only: [:show]
   end
 end
