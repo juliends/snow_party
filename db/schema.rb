eec405dc0c745d46cb78393269252b5b5b1f79e0
Dimitri Bosch <dimitribosch@hotmail.com>
2018-12-19 21:47:53 +0100

    Update schmea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_202105) do
+ActiveRecord::Schema.define(version: 2018_12_19_204710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2018_12_19_202105) do
     t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_games_on_player_id"
     t.index ["quiz_id"], name: "index_games_on_quiz_id"
+  end
+
+  create_table "leads", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
