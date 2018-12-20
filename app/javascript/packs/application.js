3868a61fa5666a860e5f8727e3f06d65a1c99eea
Germain Loret <germain.loret@gmail.com>
2018-12-20 14:53:51 +0100

    waouh.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
+import { loadDynamicBannerText } from '../components/typed';
 
 Rails.start();
 Turbolinks.start();
@@ -12,3 +13,4 @@ Turbolinks.start();
 const application = Application.start();
 const controllers = require.context('../controllers', true, /\.js$/);
 application.load(definitionsFromContext(controllers));
+loadDynamicBannerText();
