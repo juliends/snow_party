f7ee848dae13cf0708f5decf8f2ef19e15f259f8
julien da silva <julien.da.silva@hotmail.com>
2019-05-14 23:14:13 +0200

    ok

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -4,7 +4,7 @@ function loadDynamicBannerText() {
   document.addEventListener('DOMContentLoaded', ()=> {
     if (document.getElementById('banner-typed-text')) {
       new Typed('#banner-typed-text', {
-        strings: ["dans la salle de cinéma", "en avant-première"],
+        strings: ["   dans la salle de cinéma", "  en avant-première"],
         typeSpeed: 50,
         loop: true
       });
