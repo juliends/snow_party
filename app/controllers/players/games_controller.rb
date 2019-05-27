f4655886ad7d31505f59f54e5ad4a001fd6a6c72
Germain Loret <germain.loret@gmail.com>
2019-05-27 20:24:02 +0200

    start quizzes ajaxification

--- app/controllers/players/games_controller.rb
+++ app/controllers/players/games_controller.rb
@@ -3,10 +3,18 @@ class Players::GamesController < Players::BaseController
     @quiz = Quiz.find_by(playable: true)
     @game = Game.new(quiz: @quiz, player: current_player)
     authorize [:player, @game]
+    @question = @game.latest_question
+    @game_answer = GameAnswer.new(game: @game)
     if @game.save
-      redirect_to players_game_path @game
+      respond_to do |format|
+        format.html { redirect_to players_game_path @game }
+        format.js
+      end
     else
-      render "players/quizzes/show"
+      respond_to do |format|
+        format.html { render "players/quizzes/show" }
+        format.js
+      end
     end
   end
 
