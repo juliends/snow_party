b610cf19024ed0198853a0016eaf33086d240283
Germain Loret <germain.loret@gmail.com>
2019-06-03 19:56:26 +0200

    Change rules

--- app/javascript/cables/quiz_notifications.js
+++ app/javascript/cables/quiz_notifications.js
@@ -17,19 +17,22 @@ const initQuizNotification = () => {
 const displayMessage = () => {
   const container = document.getElementById('end_game_screen');
   const current_player_id = document.getElementById('player_screen').dataset.currentPlayerId;
-  const winner_id = container.dataset.winnerId;
+  // const winner_id = container.dataset.winnerId;
+  const winner_ids = container.dataset.winnerId.split(',');
+  debugger
   const winner_name = container.dataset.winnerName;
-  if (current_player_id === winner_id) {
+  // if (current_player_id === winner_id) {
+  if (winner_ids.includes(current_player_id)) {
     container.classList.add('win');
     container.innerHTML = `
       <p class="emoji">🏆</p>
       <h1>Vous avez gagné</h1>
-      <p class="content">Bravo ${winner_name}, rejoignez-nous sur scène pour recevoir votre cadeau</p>
+      <p class="content">Rejoignez-nous sur scène pour recevoir votre cadeau</p>
     `
   } else {
     container.innerHTML = `
-      <p class="emoji">👏</p>
-      <h1>${winner_name} a gagné</h1>
+      <p class="emoji">😢</p>
+      <h1>Vous avez perdu</h1>
       <p class="content">Rendez-vous à la prochaine séance pour un nouveau quiz</p>
     `
   }
