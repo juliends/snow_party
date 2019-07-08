7995c2a4b543512541a08c3326fe6e02f373685f
julien da silva <julien.da.silva@hotmail.com>
2019-07-08 22:30:39 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,82 +8,79 @@ Player.destroy_all
 
 owner = User.create(email: "dimitri@lewagon.org", password: "lewagon")
 
-quiz = Quiz.create(
-        user: owner,
-        name: "L'Homme Fidèle",
-        reward: "2 places de cinéma",
-        room: "3",
-        playable: true
-      )
+# quiz = Quiz.create(
+#         user: owner,
+#         name: "L'Homme Fidèle",
+#         reward: "2 places de cinéma",
+#         room: "3",
+#         playable: true
+#       )
 
-question1 = Question.create(content: "Votre fauteuil est-il suffisament confortable?",
-                            picture: '',
-                            quiz: quiz,
-                            category: 'confort')
-Answer.create(question: question1,
-              content: "OUI",
-              correct: true)
-Answer.create(question: question1,
-              content: "NON",
-              correct: true)
+# question1 = Question.create(content: "Votre fauteuil est-il suffisament confortable?",
+#                             picture: '',
+#                             quiz: quiz,
+#                             category: 'confort')
+# Answer.create(question: question1,
+#               content: "OUI",
+#               correct: true)
+# Answer.create(question: question1,
+#               content: "NON",
+#               correct: true)
 
-question2 = Question.create(content: "Quelle a été la fréquentation des salles 2017 ?",
-                            picture: 'https://cdn-static.denofgeek.com/sites/denofgeek/files/2016/01/o-cinema-facebook.jpg',
-                            quiz: quiz,
-                            category: 'movie')
-Answer.create(question: question2,
-              content: "145 milions",
-              correct: false)
-Answer.create(question: question2,
-              content: "195 milions",
-              correct: false)
-Answer.create(question: question2,
-              content: "210 millions",
-              correct: true)
+# question2 = Question.create(content: "Quelle a été la fréquentation des salles 2017 ?",
+#                             picture: 'https://cdn-static.denofgeek.com/sites/denofgeek/files/2016/01/o-cinema-facebook.jpg',
+#                             quiz: quiz,
+#                             category: 'movie')
+# Answer.create(question: question2,
+#               content: "145 milions",
+#               correct: false)
+# Answer.create(question: question2,
+#               content: "195 milions",
+#               correct: false)
+# Answer.create(question: question2,
+#               content: "210 millions",
+#               correct: true)
 
-question3 = Question.create(content: 'Louis Garrel a co-écrit son 1er film « Les deux amis » avec:',
-                            picture: 'http://fr.web.img4.acsta.net/c_215_290/pictures/15/07/10/15/26/039340.jpg',
-                            quiz: quiz,
-                            category: 'movie')
-Answer.create(question: question3,
-              content: "Arnaud Desplechin",
-              correct: false)
-Answer.create(question: question3,
-              content: "Christophe Honoré",
-              correct: true)
-Answer.create(question: question3,
-              content: "Philippe Garrel",
-              correct: false)
+# question3 = Question.create(content: 'Louis Garrel a co-écrit son 1er film « Les deux amis » avec:',
+#                             picture: 'http://fr.web.img4.acsta.net/c_215_290/pictures/15/07/10/15/26/039340.jpg',
+#                             quiz: quiz,
+#                             category: 'movie')
+# Answer.create(question: question3,
+#               content: "Arnaud Desplechin",
+#               correct: false)
+# Answer.create(question: question3,
+#               content: "Christophe Honoré",
+#               correct: true)
+# Answer.create(question: question3,
+#               content: "Philippe Garrel",
+#               correct: false)
 
-question4 = Question.create(content: "Avec quel réalisateur Louis Garrel a-t-il tourné ?",
-                            picture: 'https://vz.cnwimg.com/thumbc-300x300/wp-content/uploads/2014/09/Louis-Garrel1.jpg',
-                            quiz: quiz,
-                            category: 'movie')
-Answer.create(question: question4,
-              content: "Steven Spielberg",
-              correct: false)
-Answer.create(question: question4,
-              content: "Bernardo Bertolucci",
-              correct: true)
-Answer.create(question: question4,
-              content: "Fabien Onteniente",
-              correct: false)
+# question4 = Question.create(content: "Avec quel réalisateur Louis Garrel a-t-il tourné ?",
+#                             picture: 'https://vz.cnwimg.com/thumbc-300x300/wp-content/uploads/2014/09/Louis-Garrel1.jpg',
+#                             quiz: quiz,
+#                             category: 'movie')
+# Answer.create(question: question4,
+#               content: "Steven Spielberg",
+#               correct: false)
+# Answer.create(question: question4,
+#               content: "Bernardo Bertolucci",
+#               correct: true)
+# Answer.create(question: question4,
+#               content: "Fabien Onteniente",
+#               correct: false)
 
-question5 = Question.create(content: "Quel est le meilleur festival de film sous la neige ?",
-                            picture: 'https://c402277.ssl.cf1.rackcdn.com/photos/2325/images/hero_small/mountains-hero.jpg?1345838509',
-                            quiz: quiz,
-                            category: 'movie')
-Answer.create(question: question5,
-              content: "Festival des Arcs",
-              correct: true)
-Answer.create(question: question5,
-              content: "Festival Alpe d'Huez",
-              correct: false)
+# question5 = Question.create(content: "Quel est le meilleur festival de film sous la neige ?",
+#                             picture: 'https://c402277.ssl.cf1.rackcdn.com/photos/2325/images/hero_small/mountains-hero.jpg?1345838509',
+#                             quiz: quiz,
+#                             category: 'movie')
+# Answer.create(question: question5,
+#               content: "Festival des Arcs",
+#               correct: true)
+# Answer.create(question: question5,
+#               content: "Festival Alpe d'Huez",
+#               correct: false)
 
-puts "Seeded 🌱"
-
-
-
+# puts "Seeded 🌱"
 
 # Seed for Lyon
 
@@ -166,3 +163,73 @@ puts "Seeded 🌱"
 #               correct: true)
 
 # puts "Seeded 🌱"
+
+
+
+# Seed for Hirondelles
+
+quiz = Quiz.create!(
+        user: User.last,
+        name: "Les Hirondelles de Kaboul",
+        reward: "",
+        room: "1",
+        playable: true
+      )
+
+question1 = Question.create(content: "Vous allez voir “Les Hirondelles de Kaboul”. Où se déroule l’action du film ?",
+                            picture: 'photo1.jpg',
+                            quiz: quiz,
+                            category: 'movie')
+Answer.create(question: question1,
+              content: "En France",
+              correct: false)
+Answer.create(question: question1,
+              content: "Au Pakistan",
+              correct: false)
+Answer.create(question: question1,
+              content: "En Afghanistan",
+              correct: true)
+
+question2 = Question.create(content: "Comment s’appelle le vêtement que les femmes devaient porter sous le régime des talibans à Kaboul ?",
+                            picture: 'photo5.jpg',
+                            quiz: quiz,
+                            category: 'movie')
+Answer.create(question: question2,
+              content: "Le hijab (ou voile)",
+              correct: false)
+Answer.create(question: question2,
+              content: "Le Tchadri (ou la Burqa)",
+              correct: true)
+Answer.create(question: question2,
+              content: "La Djellaba",
+              correct: false)
+
+question3 = Question.create(content: "En quelle langue le livre Les hirondelles de Kaboul a-t-il été  écrit ?",
+                            picture: 'couv_livre.jpg',
+                            quiz: quiz,
+                            category: 'movie')
+Answer.create(question: question3,
+              content: "en arabe",
+              correct: false)
+Answer.create(question: question3,
+              content: "en français",
+              correct: true)
+Answer.create(question: question3,
+              content: "en dari",
+              correct: false)
+
+question4 = Question.create(content: "L’auteur du livre a signé ce roman sous le pseudonyme de Yasmina Khadra. Parmi ces écrivains célèbres, lequel n’est pas un pseudonyme ?",
+                            picture: 'yasmina_khadra.jpg',
+                            quiz: quiz,
+                            category: 'movie')
+Answer.create(question: question4,
+              content: "Simone de Beauvoir",
+              correct: true)
+Answer.create(question: question4,
+              content: "George Sand",
+              correct: false)
+Answer.create(question: question4,
+              content: "Romain Gary",
+              correct: false)
+
+puts "Seeded 🌱"
